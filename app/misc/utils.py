8be53b1e54ba1913c956ad82ddc040712e99fc34
volkrav-iMac <volodymyr.kravchenko@icloud.com>
2023-02-04 20:01:42 +0200

    use timestamp without TZ instead timestampTZ in database

--- app/misc/utils.py
+++ app/misc/utils.py
@@ -6,13 +6,13 @@ TZ = pytz.timezone("Europe/Kiev")
 
 
 async def get_now_datetime() -> datetime.datetime:
-    now = datetime.datetime.now(TZ)
+    now = datetime.datetime.now(TZ).replace(tzinfo=None)
     return now
 
 
 async def get_now_formatted() -> str:
     now_datetime = await get_now_datetime()
-    return now_datetime.strftime("%H:%M:%S %d-%m-%Y")
+    return now_datetime.strftime("%H:%M %d.%m.%Y")
 
 
 async def is_day() -> bool:
